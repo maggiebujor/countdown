0362340fd11eb0a75b61802c4cdcd87f97d45ecd
Maggie Bujor <mjb6ep@virginia.edu>
2019-04-03 18:11:25 -0400

    add githubjs file

---
+++ github.js
@@ -0,0 +1,32 @@
+/*
+Exercise for Countdown C session 3. See git_exercise.txt for details
+*/
+
+const personOne = favoriteFoods => {
+  /*insert functionality here*/
+};
+
+const personTwo = favoriteMovies => {
+  /*insert functionality here*/
+};
+
+/*you can opt out of this third function if you only have two partners*/
+const personThree = favoriteAnimals => {
+  /*insert functionality here*/
+};
+
+/*
+What does this variable syntax do?
+*/
+const favoriteFoods = [],
+  favoriteMovies = [],
+  favoriteAnimals = [];
+
+const iceBreaker = () => {
+  /*
+    Call each function here
+    */
+};
+
+/*this calls iceBreaker, which will call all of your functions*/
+iceBreaker();
